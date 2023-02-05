e44a7d8d46ea15f5f338a2619d43423d7702621b
alaidinens <alexey.laidinen@gmail.com>
2023-02-05 11:43:55 +0200

    almost

--- java.js
+++ java.js
@@ -6,19 +6,45 @@ function myDateFunction() {
   dateDiv.innerHTML = nowStr;
 }
 setInterval(myDateFunction, 1000);
-{
-  var xmlhttp = new XMLHttpRequest();
-  xmlhttp.onreadystatechange = callbackFunction(xmlhttp);
-  xmlhttp.open("GET", URL, false);
-  xmlhttp.setRequestHeader("Content-Type", "application/json");
-  xmlhttp.setRequestHeader('Authorization', 'Basic ' + window.btoa('apiusername:apiuserpassword')); //in prod, you should encrypt user name and password and provide encrypted keys here instead 
-  xmlhttp.onreadystatechange = callbackFunction(xmlhttp);
-  xmlhttp.send(ItemJSON);
-  alert(xmlhttp.responseText);
-  document.getElementById("div").innerHTML = xmlhttp.statusText + ":" + xmlhttp.status + "<BR><textarea rows='100' cols='100'>" + xmlhttp.responseText + "</textarea>";
-}
 
-function callbackFunction(xmlhttp) 
-{
-    //alert(xmlhttp.responseXML);
-}
+
+const input = document.querySelector("#input");
+const city = document.querySelector("#city");
+
+const cityName = document.querySelector("#cityName");
+const Temp = document.querySelector("#temp");
+const main = document.querySelector("#main");
+const discription = document.querySelector("#discription");
+const image = document.querySelector("#image");
+
+input.onsubmit = (e) => {
+  e.preventDefault();
+  weatherUpdate(city.value);
+  city.value = "";
+};
+
+weatherUpdate = (city) => {
+  const xhr = new XMLHttpRequest();
+  xhr.open(
+    "GET",
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=cad7ec124945dcfff04e457e76760d90`);
+  // in place of appid enter your open weather API Key
+  // You can create it for free
+  // https://home.openweathermap.org/users/sign_up
+
+  xhr.send();
+  xhr.onload = () => {
+    if (xhr.status === 404) {
+      alert("Place not found");
+    } else {
+      var data = JSON.parse(xhr.response);
+      cityName.innerHTML = data.name;
+      Temp.innerHTML = `${Math.round(data.main.temp - 273.15)}°C`;
+      main.innerHTML = data.weather[0].main;
+      discription.innerHTML = data.weather[0].description;
+      image.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    }
+  };
+};
+
+weatherUpdate("patna");
