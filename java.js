1877ff3661eeaa48b00b4aea87ff00df50a8fe74
alaidinens <alexey.laidinen@gmail.com>
2023-02-16 12:13:26 +0200

    aa

--- java.js
+++ java.js
@@ -1,49 +1,22 @@
+var input = document.querySelector('.input_text');
+var main = document.querySelector('#name');
+var temp = document.querySelector('.temp');
+var desc = document.querySelector('.desc');
+var clouds = document.querySelector('.clouds');
+var button= document.querySelector('.submit');
 
-import "./App.css";
-import { useState } from "react";
-
-const api = {
-  key: "c774716a7b3ab97e1151d8304e6b0bf9",
-  base: "https://api.openweathermap.org/data/2.5/",
-};
-
-function App() {
-  const [search, setSearch] = useState("");
-  const [weather, setWeather] = useState({});
-
-  const searchPress = () => {
-    fetch(`${api.base}weather?q=${search}&units=metric&APPID=${api.key}`)
-      .then((res) => res.json())
-      .then((result) => {
-        setWeather(result);
-      });
-  };
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Weather App</h1>
-        <div>
-          <input
-            type="text"
-            placeholder="Enter city/town..."
-            onChange={(e) => setSearch(e.target.value)}
-          />
-          <button onClick={searchPress}>Search</button>
-        </div>
-        {typeof weather.main !== "undefined" ? (
-          <div>
-            <p>{weather.name}</p>
-            <p>{weather.main.temp}°C</p>
-            <p>{weather.weather[0].main}</p>
-            <p>({weather.weather[0].description})</p>
-          </div>
-        ) : (
-          ""
-        )}
-      </header>
-    </div>
-  );
-}
-
-export default App;
+button.addEventListener('click', function(name){
+  fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=c774716a7b3ab97e1151d8304e6b0bf9')
+  .then(response => response.json())
+  .then(data => {
+    var tempValue = data['main']['temp'];
+    var nameValue = data['name'];
+    var descValue = data['weather'][0]['description'];
+  
+    main.innerHTML = nameValue;
+    desc.innerHTML = "Desc - "+descValue;
+    temp.innerHTML = "Temp - "+tempValue;
+    input.value ="";
+  })
+.catch(err => alert("Error"))
+})
